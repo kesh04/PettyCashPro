7d92f95d43c5f467ed012bc1061a3c2a23b6869c
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2026-04-20 22:02:02 +0530

    Initial Commit

---
+++ PettyCashPro/PettyCashProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PettyCashProApp.swift
+//  PettyCashPro
+//
+//  Created by Keshana Liyanaarachchi on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct PettyCashProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
